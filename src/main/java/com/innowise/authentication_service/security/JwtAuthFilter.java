2a2f2a6bc2d14ec7f30131cb1610ca82ce0d7a30
Evgeny Loshonkov <tixanuiop@gmail.com>
2026-03-22 14:53:15 +0300

    added return to JwtAuthFilter

--- src/main/java/com/innowise/authentication_service/security/JwtAuthFilter.java
+++ src/main/java/com/innowise/authentication_service/security/JwtAuthFilter.java
@@ -29,6 +29,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
+            return;
         }
 
         String token = authorizationHeader.substring(7);
