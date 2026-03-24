4ad04d21d79cc87ec43277e94c5e377e8276388d
Evgeny Loshonkov <tixanuiop@gmail.com>
2026-03-24 14:27:49 +0300

    added unit test for JwtUtil

--- src/main/java/com/innowise/authentication_service/util/JwtUtil.java
+++ src/main/java/com/innowise/authentication_service/util/JwtUtil.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.function.Function;
 
 @Component
-public class JwtUtil {
+public class JwtUtil    {
 
     private final String SECRET = "com-innowise-authentication-service-secret-key";
     private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
---
+++ src/test/java/com/innowise/authentication_service/service/AuthServiceTest.java
@@ -0,0 +1,20 @@
+package com.innowise.authentication_service.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceTest {
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void authenticate() {
+    }
+}
