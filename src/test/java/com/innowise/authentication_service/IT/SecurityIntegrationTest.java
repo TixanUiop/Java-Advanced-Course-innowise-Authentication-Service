5ce25ef8d7e9a884e22996b731a5f39b213826ad
Evgeny Loshonkov <tixanuiop@gmail.com>
2026-03-24 15:52:19 +0300

    added IT test for security config

---
+++ src/test/java/com/innowise/authentication_service/IT/SecurityIntegrationTest.java
@@ -0,0 +1,43 @@
+package com.innowise.authentication_service.IT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SecurityIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void shouldAllowAuthEndpointsWithoutToken() throws Exception {
+
+        String requestBody = """
+                {
+                    "login": "admin",
+                    "password": "admin123"
+                }
+                """;
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void shouldRejectProtectedEndpointWithoutToken() throws Exception {
+        mockMvc.perform(post("/protected"))
+                .andExpect(status().isForbidden());
+    }
+}
