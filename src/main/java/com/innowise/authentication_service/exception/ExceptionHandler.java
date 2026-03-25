94128fe5456eb1db97ca444783ed4f0e7bdee4a8
Evgeny Loshonkov <tixanuiop@gmail.com>
2026-03-25 15:58:17 +0300

    added spring security exception to handler

--- src/main/java/com/innowise/authentication_service/exception/ExceptionHandler.java
+++ src/main/java/com/innowise/authentication_service/exception/ExceptionHandler.java
@@ -2,6 +2,8 @@ package com.innowise.authentication_service.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -25,5 +27,20 @@ public class ExceptionHandler {
     @org.springframework.web.bind.annotation.ExceptionHandler(InvalidCredentials.class)
     public ResponseEntity<String> handleInvalidCredentials(InvalidCredentials ex) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<String> handleAuthException(AuthenticationException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failed: " + ex.getMessage());
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDenied(AccessDeniedException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied: " + ex.getMessage());
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleOther(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
     }
 }
