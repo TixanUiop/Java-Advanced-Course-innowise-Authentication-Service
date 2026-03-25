cbac8bde90a0f080643d661e85b5eb6987c81775
Evgeny Loshonkov <tixanuiop@gmail.com>
2026-03-25 21:07:54 +0300

    Trigger CI/CD pipeline

--- src/test/java/com/innowise/authentication_service/IT/AuthControllerIntegrationTest.java
+++ src/test/java/com/innowise/authentication_service/IT/AuthControllerIntegrationTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @Transactional
 @DisplayName("IT AuthController")
+@ActiveProfiles("test")
 class AuthControllerIntegrationTest extends BaseIntegrationTest {
 
     @Autowired
